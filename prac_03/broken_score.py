45dfc4424270675cde5d567ed564ed98d7b79f48
LukeBalfourJCU <luke.balfour@my.jcu.edu.au>
2021-08-22 23:46:04 +1000

    Refactor temperatures and broken_score programs

---
+++ prac_03/broken_score.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+import random
+
+
+def main():
+    score = random.randint(0, 100)
+    print(score)
+    result = calculate_score(score)
+    print("{}".format(result))
+
+
+def calculate_score(score):
+    if score < 51:
+        result = "Bad"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    return result
+
+main()

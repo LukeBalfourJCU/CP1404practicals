f0c71dcdc37e39788819604145593262da499d0b
jc441318 <luke.balfour@my.jcu.edu.au>
2021-08-24 13:58:29 +1000

    Add work for prac 4

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers[0]          = 3
+numbers[-1]         = 2
+numbers[3]          = 1
+numbers[:-1]        = 9, 5, 1, 4, 1, 3
+numbers[3:4]        = 1
+5 in numbers        = 4
+7 in numbers        = None
+"3" in numbers      = 0
+numbers + [6, 5, 3] = 12
+
+"""
+
+numbers[0] = "Ten"
+numbers[-1] = 1
+numbers[1:]
+9 in numbers

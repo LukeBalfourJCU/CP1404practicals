d001ffac837dd76fb252cc46d86d0208bbd5c0df
LukeBalfourJCU <luke.balfour@my.jcu.edu.au>
2021-08-29 23:05:20 +1000

    Add random gibberish that doesn't mean anything

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,13 +1,26 @@
 import random
-lottery = []
+
+"""
+I have absolutely no clue what I'm doing.
+"""
+
+def main():
+    user_input = int(input("How many quick picks? "))
+    for line in range(1, user_input):
+        generate_numbers(user_input)
+        print(result)
+
+# lottery_line = [number[0] for number in lottery]
+# for line in range(user_input):
+#    random_number = random.randint(1, 45)
+#    lottery_line.append(random_number)
+#    lottery.append(lottery_line)
 
 
-user_input = int(input("How many quick picks? "))
-print(lottery)
-
-
-lottery_line = [number[0] for number in lottery]
-for line in range(user_input):
+def generate_numbers(user_input):
     random_number = random.randint(1, 45)
-    lottery_line.append(random_number)
-    lottery.append(lottery_line)
+    lottery_line = str((random_number + " ") * user_input)
+    result = str(lottery_line)
+    return result
+
+main()
